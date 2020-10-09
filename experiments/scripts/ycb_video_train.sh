5865f168164be1b76ce91fdb2ffd4ca06b51cbac
yuxng <yux@nvidia.com>
2020-10-09 16:51:48 -0700

    change script

--- experiments/scripts/ycb_video_train.sh
+++ experiments/scripts/ycb_video_train.sh
@@ -3,9 +3,6 @@
 set -x
 set -e
 
-export PYTHONUNBUFFERED="True"
-export CUDA_VISIBLE_DEVICES=0,1
-
 time ./tools/train_net.py \
   --network posecnn \
   --pretrained data/checkpoints/vgg16-397923af.pth \
