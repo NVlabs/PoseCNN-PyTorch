c443a7a8db00832bf8bfca84b40622af7a5a8727
yuxng <yux@nvidia.com>
2020-10-09 16:42:28 -0700

    change ngc script

--- ngc/train_ngc.sh
+++ ngc/train_ngc.sh
@@ -14,9 +14,9 @@ ngc batch run \
     --name "$NAME" \
     --image "nvcr.io/nvidian/robotics/posecnn-pytorch:latest" \
     --result /result \
-    --datasetid "58777:/posecnn/data/models" \
-    --datasetid "58774:/posecnn/data/backgrounds" \
-    --datasetid "8187:/posecnn/data/coco" \
+    --datasetid "58777:/posecnn-release/data/models" \
+    --datasetid "58774:/posecnn-release/data/backgrounds" \
+    --datasetid "8187:/posecnn-release/data/coco" \
     --datasetid "11888:/posecnn-release/data/YCB_Video/YCB_Video_Dataset" \
     --workspace posecnn-release:/posecnn-release \
     --commandline "cd /posecnn-release; bash $SCRIPT" \
