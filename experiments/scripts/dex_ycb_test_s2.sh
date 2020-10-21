483ec59e951d677a833d0ebf4ba94ea6dbf88cf4
yuxng <yux@nvidia.com>
2020-10-20 21:08:36 -0700

    add scripts

--- experiments/scripts/dex_ycb_test_s2.sh
+++ experiments/scripts/dex_ycb_test_s2.sh
@@ -6,6 +6,6 @@ export CUDA_VISIBLE_DEVICES=$1
 
 time ./tools/test_net.py --gpu $1 \
   --network posecnn \
-  --pretrained output/dex_ycb/dex_ycb_s0_train/vgg16_dex_ycb_epoch_$2.checkpoint.pth \
-  --dataset dex_ycb_s0_test \
+  --pretrained output/dex_ycb/dex_ycb_s2_train/vgg16_dex_ycb_epoch_$2.checkpoint.pth \
+  --dataset dex_ycb_s2_test \
   --cfg experiments/cfgs/dex_ycb.yml
